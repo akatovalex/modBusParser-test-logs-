07c030e807e4af77eaebfa0466fcb7cf7b16b241
akatovalex <akatovaleksander@gmail.com>
2021-11-26 14:59:44 +0300

    UI modified, Commands and MBExceptions classes aded

---
+++ Commands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace modBusParse {
+    class Commands {
+
+        public Commands(string commandsFilePath) {
+            Dictionary<string, string> b = new Dictionary<string, string>();
+
+            string[] fs = File.ReadAllLines(commandsFilePath);
+            foreach(string inputLine in fs) {
+                var formatInput = inputLine.Trim().Split('-');
+                b.Add(formatInput[0].Trim(), formatInput[1].Trim());
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,41 +20,90 @@ namespace modBusParse {
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        int outputFormat;
+
         public MainWindow() {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e) {
+
+        private void BtnFileOpen_Click(object sender, RoutedEventArgs e) {
             var fileDialog = new System.Windows.Forms.OpenFileDialog();
             var result = fileDialog.ShowDialog();
             switch (result) {
-                case System.Windows.Forms.DialogResult.OK:
+                case System.Windows.Forms.DialogResult.OK: {
                     var file = fileDialog.FileName;
-                    TxtFile.Text = file;
-                    TxtFile.ToolTip = file;
+                    label_FilePath.Content = file;
+                    label_FilePath.ToolTip = file;
+                    btn_CreateLog.IsEnabled = true;
                     break;
+                }
                 case System.Windows.Forms.DialogResult.Cancel:
-                default:
-                    TxtFile.Text = null;
-                    TxtFile.ToolTip = null;
+                default: {
+                    label_FilePath.Content = null;
+                    label_FilePath.ToolTip = null;
+                    btn_CreateLog.IsEnabled = false;
                     break;
+                }
             }
         }
 
-        private void BtnFileOpen_Click(object sender, RoutedEventArgs e) {
-
-        }
-
         private void Rb_JSON_Checked(object sender, RoutedEventArgs e) {
-
+            outputFormat = 0;
         }
 
         private void Rb_XML_Checked(object sender, RoutedEventArgs e) {
-
+            outputFormat = 1;
         }
 
         private void Rb_TXT_Checked(object sender, RoutedEventArgs e) {
-
+            outputFormat = 2;
         }
+
+        private void Btn_CreateLog_Click(object sender, RoutedEventArgs e) {
+            switch (this.outputFormat) {
+                case 0: {
+
+                    break;
+                }
+                case 1: {
+
+                    break;
+                }
+                default: {
+
+                    break;
+                }
+            }
+        }
+
+        private void Btn_CreateLogFromRawData_Click(object sender, RoutedEventArgs e) {
+            switch (this.outputFormat) {
+                case 0: {
+
+                    break;
+                }
+                case 1: {
+
+                    break;
+                }
+                default: {
+
+                    break;
+                }
+            }
+        }
+
+
+        private void TxtFile_TextChanged(object sender, TextChangedEventArgs e) {
+            if (TxtFile.Text != "") {
+                btn_CreateLogFromRawData.IsEnabled = true;
+            }
+            else {
+                btn_CreateLogFromRawData.IsEnabled = false;
+            }
+        }
+
     }
 }
